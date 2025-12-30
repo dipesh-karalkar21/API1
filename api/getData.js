5730603ce65973eb3237303471239d5e40eb9e6a
dipesh-karalkar21 <82707684+dipesh-karalkar21@users.noreply.github.com>
2025-12-30 19:17:54 +0530

    Update getData.js

--- api/getData.js
+++ api/getData.js
@@ -25,12 +25,13 @@ module.exports = async(req,res)=>{
     console.log(data.data.values)
 
     res.setHeader("Access-Control-Allow-Origin","*")
-    res.setHeader("Access-Control-Allow-Methods","GET,POST,OPTIONS")
+    res.setHeader("Access-Control-Allow-Methods","GET","POST","OPTIONS")
     res.setHeader("Access-Control-Allow-Headers","Content-Type")
     if (req.method === "OPTIONS") {
         return res.status(200).end();
     }
     
     return res.json(data.data.values)
+
 
 }
